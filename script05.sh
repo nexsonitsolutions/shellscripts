2eb14931b922cc2e3019e72405b4b678e8e1ded7
prashanth <nexsonitsolutions@gmail.com>
2022-05-16 02:57:19 +0000

    second commit - script05.sh

---
+++ script05.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+# Description: This script prints Hello world 5 times
+# Author: Prashanth <projects2488@gmail.com>
+# Date Created: 
+# Date Modified:  
+# Version: 
+echo "hello there"
+for (( i=0;i<6;i++ ))
+do
+	echo 'Hello World'
+done
